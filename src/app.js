c8a4f885f5c77572cd9a760c0fb2cf1ed3b73c93
Renan Lucas <renanluquinhas53@gmail.com>
2020-08-22 23:10:36 -0300

    Desafio concluído

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,67 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+    const { title, url, techs } = request.body;
+
+    const id = uuid();
+
+    const project = { id, title, url, techs, likes: 0 };
+
+    repositories.push(project);
+
+    return response.json(project);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+    const { id } = request.params;
+
+    const { title, url, techs } = request.body;
+
+    const project = repositories.find(repo => repo.id === id);
+
+    if (!project) {
+      return response.status(400).json({ error: 'Project not found' });
+    }
+
+    const projectIndex = repositories.findIndex(repo => repo.id === id);
+
+    const newProject = { id, title, url, techs, likes: project.likes };
+
+    repositories[projectIndex] = newProject;
+
+    return response.json(newProject);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+    const { id } = request.params;
+
+    const projectIndex = repositories.findIndex(project => project.id === id);
+
+    if (projectIndex < 0) {
+      return response.status(400).json({ error: 'Project not found' });
+    }
+
+    repositories.splice(projectIndex, 1);
+
+    return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+    const { id } = request.params;
+
+    const project = repositories.find(repo => repo.id === id);
+
+    if (!project) {
+      return response.status(400).json({ error: 'Project not found to like' });
+    }
+
+    project.likes += 1;
+
+    return response.json({ likes: project.likes });
+}); 
 
 module.exports = app;
